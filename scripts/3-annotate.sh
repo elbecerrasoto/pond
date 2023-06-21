0a281b7cb1e43edb11cd6b4db029addf4fb28aa2
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-06-20 20:45:07 -0500

    annotate all filtered

--- scripts/3-annotate.sh
+++ scripts/3-annotate.sh
@@ -1,36 +1,45 @@
 #!/usr/bin/env zsh
 
-# for my machine this settings work
-# 6 batch x 7 cpu
-# doing 256
+# Assumes that
+# interproscan.sh is installed
 
 genome="$1"
 out_dir="$2"
-cpus="$3" # cpus per iscan
-iscan_bin="/home/ebecerra/4-env/bin/source/interproscan-5.61-93.0/interproscan.sh"
+cpus="$3"
+pfam_only=${4:-TRUE}
 
 
-if [[ "$#" !=  3 ]]
+if [[ "$#" !=  3 ]] && [[ "$#" != 4 ]]
 then
-    printf 'Args missing\n' >&2
-    printf 'This script takes two positional args\n' >&2
-    printf 'genome.faa and out_dir\n' >&2
+    printf 'Wrong argument number: accepting only 3 or 4\n' >&2
+    printf "$0 genome out_dir cpus [pfam_only: TRUE | FALSE]\n" >&2
     exit 1
 fi
 
 
-if [[ ! -f "$iscan_bin" ]]
+if [[ "$pfam_only" != "TRUE" ]] && [[ "$pfam_only" != "FALSE" ]]
 then
-    printf 'NO interproscan binary\n' >&2
-    exit 2
+    printf 'Wrong argument: pfam_only must be TRUE or FALSE\n' >&2 
+    exit 1
 fi
 
-annotation=`fd -d 1 -e xml . $out_dir`
+
+mkdir -p "$out_dir"
+annotation="`fd -d 1 -e xml . $out_dir`"
+
 
 # If there is NOT an annotation
 if [[ -z "$annotation" ]]
 then
-    mkdir -p "$out_dir"
-    # --disable-precalc good on a many analyses, bad on single
-    "$iscan_bin" --cpu "$cpus" --input "$genome" --output-dir "$out_dir"
+    if [[ "$pfam_only" == "TRUE" ]]
+    then
+
+        interproscan.sh --cpu $cpus --input $genome --output-dir $out_dir ---disable-precalc --applications Pfam
+
+    elif [[ "$pfam_only" == "FALSE" ]]
+    then
+
+        interproscan.sh --cpu $cpus --input $genome --output-dir $out_dir --disable-precalc
+
+    fi
 fi
