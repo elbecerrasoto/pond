68bc7244b3c91358b232c37f6b8547f7374edec7
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-06-29 19:13:03 -0500

    wip download_genomes

--- scripts/1-download_genome/download_genome.py
+++ scripts/1-download_genome/download_genome.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+# Depends on ncbi-datasets-cli
+# Install with
+# mamba install -y -c conda-forge ncbi-datasets-cli
+
 import argparse
 import subprocess as sp
 from pathlib import Path
 from icecream import ic
+from shlex import split, join
 
 parser = argparse.ArgumentParser()
 parser.add_argument("genome", help="Some help")
@@ -19,15 +24,21 @@ ZIP = OUT_DIR / (GENOME + ".zip")
 DRY = args.dry_run
 DEBUG = args.debug
 
-CMD = f"datasets download genome accession {GENOME} --filename {ZIP}"
-
+CMD = split(f"datasets download genome accession {GENOME} --filename {ZIP}")
 
 if __name__ == "__main__":
     if DEBUG:
         ic(args)
+        ic(ZIP)
+        ic(CMD)
 
     if not DRY:
         OUT_DIR.mkdir(parents=True, exist_ok=True)
+        sp.run(CMD, check=True)
+        # unzip
+        # remames
+        # removes
     else:
         print("DRY RUN\nActions that would've run:\n")
         print(f"mkdir -p {OUT_DIR}")
+        print(join(CMD))
