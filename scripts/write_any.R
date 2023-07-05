adaf3b24bc36f83adebfc46353683e84eb70c8ec
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-07-04 20:01:28 -0500

    write_any working on hard coded data

--- scripts/write_any.R
+++ scripts/write_any.R
@@ -1,38 +1,42 @@
 #!/usr/bin/env Rscript
 
-# Redirect stderr to /dev/null
-SUPRESS_STDERR = T
+suppressMessages(library(seqinr))
 suppressMessages(library(tidyverse))
 library(argparse)
 
 
-parser <- ArgumentParser(description="Print cmd to extract proteins from a FASTA\n
-                                      the filteriing is done\n
-                                      using IntreproScan 5 TSV and a TXT of Interpro IDs")
+## parser <- ArgumentParser(description="Print cmd to extract proteins from a FASTA\n
+##                                       the filteriing is done\n
+##                                       using IntreproScan 5 TSV and a TXT of Interpro IDs")
 
-parser$add_argument("tsv", type="character", nargs=1,
-                    help="File containing the annotation from InterProScan 5 on TSV")
+## parser$add_argument("tsv", type="character", nargs=1,
+##                     help="File containing the annotation from InterProScan 5 on TSV")
 
 
-parser$add_argument("ids", type="character", nargs=1,
-                    help="File of the interpro IDs to filter,\n
-                    one interpro ID per line, use '#' to comment\n ")
+## parser$add_argument("ids", type="character", nargs=1,
+##                     help="File of the interpro IDs to filter,\n
+##                     one interpro ID per line, use '#' to comment\n ")
 
-parser$add_argument("faa", type="character", nargs=1,
-                    help="FASTA aminoacid to filter")
+## parser$add_argument("faa", type="character", nargs=1,
+##                     help="FASTA aminoacid to filter")
 
-ARGS <- parser$parse_args()
+## ARGS <- parser$parse_args()
 
-if (SUPRESS_STDERR)
-{
-  null <- file(nullfile(), open="w")
-  sink(null, type="message")
-  
-}
+## if (SUPRESS_STDERR)
+## {
+##   null <- file(nullfile(), open="w")
+##   sink(null, type="message")
 
-TSV <- ARGS$tsv
-IDS <- read_tsv(ARGS$ids, comment = "#", col_names = FALSE)[[1]]
-FAA <- ARGS$faa
+## }
+
+## TSV <- ARGS$tsv
+## IDS <- read_tsv(ARGS$ids, comment = "#", col_names = FALSE)[[1]]
+## FAA <- ARGS$faa
+
+TSV <- "GCF_001991475.1.faa.tsv"
+IDS <- "../input_pfams.txt"
+FAA <- "GCF_001991475.1.faa"
+
 
 HEADER <- c("protein",
             "md5",
@@ -49,24 +53,26 @@ HEADER <- c("protein",
             "interpro_txt")
 
 
-annotation <- read_tsv(TSV, col_names=HEADER)
+annotation <- read_tsv(TSV, col_names=HEADER, show_col_types = FALSE)
 
-tmp <- paste0(TSV, ".hits")
 
-# ANY hit
-annotation %>%
-  filter(interpro %in% IDS) %>%
-  distinct(protein) %>%
-  write_tsv(file = tmp, col_names = FALSE)
-
-out <-  paste0(FAA, ".tox.faa")
-cmd <- paste0("fasta_grep --tab=",
-              tmp, " ", FAA, " >| ", out)
-# just print the command to avoid complexities
-# of running it from this R script
-cat(cmd)
-
-if (SUPRESS_STDERR)
+parse_ids <- function (path2ids)
 {
-  sink()
+  read_tsv(IDS, comment = "#", col_names = "interpro", col_types = "c")[[1]]
 }
+
+ids <- parse_ids(IDS)
+
+hits <-
+annotation %>%
+  filter(interpro %in% ids) %>%
+  distinct(protein)
+
+hits <- hits[[1]]
+
+faa <- read.fasta(FAA, seqtype="AA", forceDNAtolower = FALSE, strip.desc = TRUE)
+faa_hits <- faa[hits]
+
+faa_hits_headers = map(faa_hits, attr, which = "Annot")
+# nbchar = 80 to make it equal to ncbi source
+write.fasta(faa_hits, names = faa_hits_headers, nbchar = 80, file.out = "test.faa")
