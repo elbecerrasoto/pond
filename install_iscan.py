8cfb102ab83501304a5a404be9c0c61831273e73
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-08-01 14:02:31 -0500

    wip script to download install iscan

---
+++ install_iscan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+# iscan globals
+ISCAN_VERSION = "5.63-95.0"
+ISCAN_INSTALLATION_DIR = Path("/usr/share/interproscan")
+
+# remotes
+ISCAN_FTP = f"https://ftp.ebi.ac.uk/pub/databases/interpro/iprscan/5/{ISCAN_VERSION}"
+ISCAN_FTP_GZ = f"{ISCAN_FTP}/interproscan-{ISCAN_VERSION}-64-bit.tar.gz"
+ISCAN_FTP_MD5 = f"{ISCAN_FTP_GZ}.md5"
+
+# local
+MD5 = ISCAN_INSTALLATION_DIR / Path(ISCAN_FTP_MD5).name
+GZ = ISCAN_INSTALLATION_DIR / Path(ISCAN_FTP_GZ).name
+ISCAN_BIN = ISCAN_INSTALLATION_DIR / f"interproscan-{ISCAN_VERSION}/interproscan.sh"
+
+
+print(ISCAN_BIN)
+
+from subprocess import run
+from shlex import split, join
+
+cmd_aria2 = split(
+    f"aria2c --continue=true --split 12 --max-connection-per-server=16 --min-split-size=1M {ISCAN_FTP_MD5}"
+)
+
+print(f"Running \n{join(cmd_aria2)}")
+run(cmd_aria2, check=True)
