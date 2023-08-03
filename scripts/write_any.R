acb0effd5b9396570e6c0584a84ea6d8801b46ff
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-08-03 14:40:13 -0500

    suppress warning 13 rows expected 15

--- scripts/write_any.R
+++ scripts/write_any.R
@@ -5,37 +5,32 @@ suppressMessages(library(tidyverse))
 library(argparse)
 
 
-## parser <- ArgumentParser(description="Print cmd to extract proteins from a FASTA\n
-##                                       the filteriing is done\n
-##                                       using IntreproScan 5 TSV and a TXT of Interpro IDs")
+parser <- ArgumentParser(description="Print cmd to extract proteins from a FASTA\n
+                                      the filteriing is done\n
+                                      using IntreproScan 5 TSV and a TXT of Interpro IDs")
 
-## parser$add_argument("tsv", type="character", nargs=1,
-##                     help="File containing the annotation from InterProScan 5 on TSV")
+parser$add_argument("tsv", type="character", nargs=1,
+                    help="File containing the annotation from InterProScan 5 on TSV")
 
 
-## parser$add_argument("ids", type="character", nargs=1,
-##                     help="File of the interpro IDs to filter,\n
-##                     one interpro ID per line, use '#' to comment\n ")
+parser$add_argument("ids", type="character", nargs=1,
+                    help="File of the interpro IDs to filter,\n
+                    one interpro ID per line, use '#' to comment\n ")
 
-## parser$add_argument("faa", type="character", nargs=1,
-##                     help="FASTA aminoacid to filter")
+parser$add_argument("faa", type="character", nargs=1,
+                    help="FASTA aminoacid to filter")
 
-## ARGS <- parser$parse_args()
+# ARGS <- parser$parse_args()
 
-## if (SUPRESS_STDERR)
-## {
-##   null <- file(nullfile(), open="w")
-##   sink(null, type="message")
 
-## }
+# TSV <- ARGS$tsv
+# IDS <- ARGS$ids
+# FAA <- ARGS$faa
 
-## TSV <- ARGS$tsv
-## IDS <- read_tsv(ARGS$ids, comment = "#", col_names = FALSE)[[1]]
-## FAA <- ARGS$faa
 
-TSV <- "GCF_001991475.1.faa.tsv"
-IDS <- "../input_pfams.txt"
-FAA <- "GCF_001991475.1.faa"
+TSV <- "../2-pfams/GCF_001991475.1.pfam.tsv"
+IDS <-  "../input_pfams.txt"
+FAA <- "../1-genomes/GCF_001991475.1/GCF_001991475.1.faa"
 
 
 HEADER <- c("protein",
@@ -50,10 +45,9 @@ HEADER <- c("protein",
             "status",
             "date",
             "interpro",
-            "interpro_txt")
-
-
-annotation <- read_tsv(TSV, col_names=HEADER, show_col_types = FALSE)
+            "interpro_txt",
+            "GOs",
+            "pathways")
 
 
 parse_ids <- function (path2ids)
@@ -61,7 +55,10 @@ parse_ids <- function (path2ids)
   read_tsv(IDS, comment = "#", col_names = "interpro", col_types = "c")[[1]]
 }
 
+# problems(annotation) some rows with 13 rows, expected 15
+annotation <- suppressWarnings(read_tsv(TSV, col_names=HEADER, show_col_types = FALSE))
 ids <- parse_ids(IDS)
+
 
 hits <-
 annotation %>%
